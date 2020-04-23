5b4a37b17be9531bae1fd27cafac75a1c311618f
GIuseppe D'Ambrosio <kristallon@pop-os.localdomain>
2020-04-23 02:46:25 +0200

    Updates

---
+++ precache-manifest.8556de4f58effe0d7b20dc75b1d10295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad597d473c98e2a5896caf16783c95e",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "1256aa32a75d7fee1899",
+    "url": "/client/static/css/main.94a0dfaa.chunk.css"
+  },
+  {
+    "revision": "caac2c176d12631f25a2",
+    "url": "/client/static/js/2.57aefcae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/client/static/js/2.57aefcae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1256aa32a75d7fee1899",
+    "url": "/client/static/js/main.b0818588.chunk.js"
+  },
+  {
+    "revision": "78fa274c5a6a557ea74f",
+    "url": "/client/static/js/runtime-main.b5bbee55.js"
+  }
+]);
